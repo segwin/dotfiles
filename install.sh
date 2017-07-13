d6d62b54f41f03c4dc678c46f5ab802694968564
Louis Poudrier-Racette <lpracette@gmail.com>
2017-07-13 12:55:19 -0400

    Remove oh-my-tmux

--- install.sh
+++ install.sh
@@ -3,14 +3,11 @@
 DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
 
 
-
-DOTFILES_TO_INSTALL="bin .subversion .gitconfig .tmux.conf.local .vimrc .zshrc .bashrc .config/ranger/rc.conf .config/ranger/scope.sh dircolors.256dark .pythonrc"
-
-DOTFILES_TO_INSTALL_i3=" .config/i3/config .config/i3/conky.conf .config/i3/conky-wrapper .config/lxterminal/lxterminal.conf .conky .conkyrc .w3m/config .Xresources"
-
+DOTFILES_TO_INSTALL="bin .subversion .gitconfig .tmux.conf .vimrc .zshrc .bashrc .config/ranger/rc.conf .config/ranger/scope.sh .dircolors.256dark .pythonrc .w3m/config"
+DOTFILES_TO_INSTALL_i3=" .config/i3/config .config/i3/conky.conf .config/i3/conky-wrapper .config/lxterminal/lxterminal.conf .conky .conkyrc .Xresources"
 
 mkdir -p $HOME/.config/ranger
-
+mkdir -p $HOME/.w3m
 
 function confirm()
 {
@@ -22,6 +19,12 @@ function confirm()
     return 1
 }
 
+if confirm "Use oh-my-zsh"; then
+    if [ ! -d $DOTFILES/oh-my-zsh ]; then 
+        export ZSH="$DOTFILES/oh-my-zsh"; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    fi
+fi
+
 for f in $DOTFILES_TO_INSTALL; do
     if [ -h $HOME/$f ]; then
         if [ $(readlink $HOME/$f) = $DOTFILES/$f ]; then
@@ -31,29 +34,15 @@ for f in $DOTFILES_TO_INSTALL; do
         if [ -f $HOME/$f ]; then
             if confirm "Overwrite $HOME/$f?"; then
                 rm -rf $HOME/$f
-                ln -s $DOTFILES/$f $HOME/$f
+                ln -sf $DOTFILES/$f $HOME/$f
                 echo $HOME/$f installed!
             fi
         else
-            ln -s $DOTFILES/$f $HOME/$f
+            ln -sf $DOTFILES/$f $HOME/$f
             echo $HOME/$f installed!
         fi
     fi
 done
-
-# install oh-my-zsh
-if [ -z "$STATE" ]; then
-    if [ ! -d $DOTFILES/oh-my-zsh ]; then 
-        export ZSH="$DOTFILES/oh-my-zsh"; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    fi
-fi
-
-# install on-my-tmmux
-ln -sf $DOTFILES/.tmux/.tmux.conf
-# cd $HOME
-# if confirm "Use oh-my-tmux"; then
-#     cp $DOTFILES/.tmux/.tmux.conf.local $
-# fi
 
 if confirm "Install i3 dotfiles"; then
     for f in $DOTFILES_TO_INSTALL_i3; do
