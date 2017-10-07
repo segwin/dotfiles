3b4dde1403be8ece30c0e9013447ba190daa5546
Louis Poudrier-Racette <lpracette@gmail.com>
2017-10-07 18:18:46 -0400

    Tmux status line

--- bin/ping_latency.sh
+++ bin/ping_latency.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-printf '%5s\n' $(ping -c1 -W1 $1 | awk -F'=' '/time/{print $4}'|cut -f 1 -d ' ')
+if ping -c1 -W1 $1 >/dev/null 2>&1; then
+    printf ' %5s \n' $(ping -c1 -W1 $1 | awk -F'=' '/time/{print $4}'|cut -f 1 -d ' ')
+fi
