45b6a70051f49d84ca6cc5e2379a990eeab2fb36
Louis Poudrier-Racette <lpracette@gmail.com>
2017-10-11 00:14:51 -0400

    Non powerline theme for tmux

--- bin/ping_latency.sh
+++ bin/ping_latency.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if ping -c1 -W1 $1 >/dev/null 2>&1; then
-    printf ' %5s \n' $(ping -c1 -W1 $1 | awk -F'=' '/time/{print $4}'|cut -f 1 -d ' ')
+    printf " %5s $2\n" $(ping -c1 -W1 $1 | awk -F'=' '/time/{print $4}'|cut -f 1 -d ' ')
 fi
